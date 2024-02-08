fb38ecf4e0540371a7a18cfa3ef7bc8866389c7f
Florian Goussin <floriangoussin@floriangoussin.local>
2024-02-08 10:38:32 -0700

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet } from 'react-native'
 import Search from './components/Search'
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1
+  }
+})
 
 export default function App() {
   const insets = useSafeAreaInsets()
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1
-        // paddingTop: insets.top,
-        // paddingBottom: insets.bottom
-      }}
-    >
+    <SafeAreaView style={styles.view}>
       <Search />
       <StatusBar style="auto" />
     </SafeAreaView>
   )
 }
-
-// const styles = StyleSheet.create({})
