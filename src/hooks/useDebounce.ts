81f65180173fb53a78d536cc8ba034b37a64edb3
Florian Goussin <floriangoussin@floriangoussin.local>
2024-02-08 10:34:29 -0700

    fix hook

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,17 +1,15 @@
 import { debounce } from 'lodash'
 import { useEffect, useRef, useMemo } from 'react'
 
-export const useDebounce = (callback: Function, delay: number = 300) => {
-  const ref = useRef<Function>(callback)
+export const useDebounce = (callback: () => void, delay: number = 300) => {
+  const ref = useRef(callback)
 
   useEffect(() => {
     ref.current = callback
   }, [callback])
 
   const debounced = useMemo(() => {
-    return debounce(() => {
-      ref.current()
-    }, delay)
+    return debounce(() => ref.current(), delay)
   }, [])
 
   return debounced
